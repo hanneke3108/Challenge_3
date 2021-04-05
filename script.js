408cb7c33829f26ec8add5580569978b3ee44b8c
Hanneke <hanneke3108@gmail.com>
2021-04-05 13:17:26 +0200

    added geocoder
    Added geocoder, but doesn't work like I wanted yet. And deleted the comments.

--- script.js
+++ script.js
@@ -88,154 +88,26 @@ function getMapLocation(response) {
         style: 'mapbox://styles/mapbox/satellite-v9',
         container: 'map',
         center: [weatherLoc.lon, weatherLoc.lat],
-        //[4.322840, 52.067101]
-        //bearing: -45,
         pitch: 0,
         zoom: 12,
     });
+    
+    //HOE PAS IK GEOCODER TOE MET EIGEN INPUT
+    var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
 
-map.addControl(new mapboxgl.NavigationControl()); 
+    // Voeg de zoekbalk toe
+    map.addControl( geocoder);
+
+    map.on('load', function () {
+        // Listen for the `geocoder.input` event that is triggered when a user
+        // makes a selection
+        geocoder.on('result', function (ev) {
+        // console.log(ev.result.center);
+        });
+    });
+
+    map.addControl(new mapboxgl.NavigationControl()); 
 }
-
-
-//var HHSpopup = new mapboxgl.Popup().setHTML('<h3>De Haagse Hogeschool</h3><p>Is momenteel dicht.</p>');
-//
-//// Adding a marker based on lon lat coordinates
-//var marker = new mapboxgl.Marker()
-//  .setLngLat([30.7658, 20.1653])
-//  .setPopup(HHSpopup)
-//  .addTo(map);
-
-
-
-//// wacht tot de map en styles geladen zijn
-//map.on('load', function () {
-//
-//  // laad een extern bestand
-//  map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function (error, image) {
-//
-//      // voeg image toe en noem het cat
-//      map.addImage('cat', image);
-//
-//      // defineer een punt in het geheugen
-//      map.addSource('point', {
-//        type: 'geojson',
-//        data: {
-//          type: 'FeatureCollection',
-//          features: [{
-//            type: 'Feature',
-//            geometry: {
-//              type: 'Point',
-//              coordinates: [4.32284, 52.067101]
-//            }
-//          }]
-//        }
-//      });
-//
-//      // plak de nieuwe source 'point' op de kaart in een eigen layer
-//      map.addLayer({
-//        id: 'points',
-//        type: 'symbol',
-//        source: 'point',
-//        layout: {
-//          'icon-image': 'cat',
-//          'icon-size': 0.25
-//        }
-//      });
-//    }
-//  );
-//});
-
-
-
-//// wacht totdat de map geladen is
-//map.on('load', function () {
-//
-//  // nieuwe source places
-//  map.addSource('places', {
-//    'type': 'geojson',
-//    'data': {
-//      'type': 'FeatureCollection',
-//      // Saved in locations.js
-//      'features': myLocationsList
-//    }
-//  });
-//
-//  // Add a layer showing the places.
-//  map.addLayer({
-//    'id': 'places',
-//    'type': 'symbol',
-//    'source': 'places',
-//    'layout': {
-//      'icon-image': '{icon}-15', // icon verwijst naar de properties in het object met een plaats en wordt vervangen
-//      'icon-allow-overlap': true
-//    }
-//  });
-//});
-
-
-
-//map.on('load', function () {
-//  map.addSource('places', {
-//    'type': 'geojson',
-//    'data': {
-//      'type': 'FeatureCollection',
-//      'features': myLocationsList
-//    }
-//  });
-//
-//  // Add a layer showing the places.
-//  map.addLayer({
-//    'id': 'places',
-//    'type': 'symbol',
-//    'source': 'places',
-//    'layout': {
-//      'icon-image': '{icon}-15',
-//      'icon-allow-overlap': true
-//    }
-//  });
-//
-//  // Create a popup, but don't add it to the map yet.
-//  var popup = new mapboxgl.Popup({
-//    closeButton: false,
-//    closeOnClick: false
-//  });
-//
-//  map.on('mouseenter', 'places', function (e) {
-//    var coordinates = e.features[0].geometry.coordinates.slice();
-//    var description = e.features[0].properties.description;
-//
-//    // Populate the popup and set its coordinates based on the feature found.
-//    popup.setLngLat(coordinates)
-//         .setHTML(description)
-//         .addTo(map);
-//  });
-//
-//  map.on('mouseleave', 'places', function () {
-//    popup.remove();
-//  });
-//});
-
-
-
-//// Voeg de zoekbalk toe
-//map.addControl(
-//  new MapboxGeocoder({
-//    accessToken: mapboxgl.accessToken,
-//    mapboxgl: mapboxgl
-//  }),
-//  'top-right'
-//);
-
-
-
-//// Voeg de planner toe
-//map.addControl(
-//  new MapboxDirections({
-//    accessToken: mapboxgl.accessToken
-//  }),
-//  'top-left'
-//);
-
-
-
